a4b310d7d4b5b52acbe90cd393bbc9e4f31b8b4e
Mészáros Mátyás <34501282+mmatyi@users.noreply.github.com>
2021-05-20 13:56:36 +0200

    Update install.R

--- .binder/install.R
+++ .binder/install.R
@@ -1,1 +1,1 @@
-install.packages(c("tm", "ggplot2", "dplyr","tidytext","topicmodels","wordcloud2","openxlsx","textmineR","tidyr","repr","Hmisc","geojsonio"), repos="https://cloud.r-project.org")
+install.packages(c("tm", "ggplot2", "dplyr","tidytext","topicmodels","wordcloud2","openxlsx","textmineR","tidyr","repr"), repos="https://cloud.r-project.org")
